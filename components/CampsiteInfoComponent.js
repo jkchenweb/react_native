c5bf6baff83f11740dcf923b585658abdb5008be
Jeff Chen <jkchenweb@gmail.com>
2020-12-05 07:40:30 -0800

    Gestures Part 1

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, FlatList,
+    Modal, Button, StyleSheet,
+    Alert, PanResponder } from 'react-native';
 import { Card, Icon, Input, Rating } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -7,6 +9,7 @@ import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite } from '../redux/ActionCreators';
 import { postComment} from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -25,9 +28,43 @@ function RenderCampsite(props) {
 
     const {campsite} = props;
 
+    const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if (recognizeDrag(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () => props.favorite ?
+                                console.log('Already set as a favorite') : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+            return true;
+        }
+    });
+
     if (campsite) {
         return (
-            <Card
+            <Animatable.View
+                animation='fadeInDown'
+                duration={2000}
+                delay={1000}
+                {...panResponder.panHandlers}>
+                    <Card
                 featuredTitle={campsite.name}
                 image={{uri: baseUrl + campsite.image}}>
                 <Text style={{margin: 10}}>
@@ -53,7 +90,7 @@ function RenderCampsite(props) {
                     />
                 </View>
             </Card>
-
+            </Animatable.View>
         );
     }
     return <View style={{margin:10}} />;
@@ -77,13 +114,15 @@ function RenderComments({comments}) {
     };
 
     return (
-        <Card title='Comments'>
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title='Comments'>
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        </Animatable.View>
     );
 }
 
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
     constructor(props) {
@@ -17,19 +18,18 @@ class Contact extends Component {
 
     render() {
         return (
-        <ScrollView>
-            <Card title="Contact Information">
-                
-                <Text>1 Nucamp Way</Text>
-                <Text>Seattle, WA 98001</Text>
-                <Text style={{marginBottom: 10}}>U.S.A.</Text>
-                <Text>Phone: 1-206-555-1234</Text>
-                <Text>Email: campsites@nucamp.co</Text>
-
-
-            </Card>
-        </ScrollView>
-        )
+            <ScrollView>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>          
+                    <Card title="Contact Information">
+                        <Text>1 Nucamp Way</Text>
+                        <Text>Seattle, WA 98001</Text>
+                        <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                        <Text>Phone: 1-206-555-1234</Text>
+                        <Text>Email: campsites@nucamp.co</Text>
+                    </Card>
+                </Animatable.View>
+            </ScrollView>
+        );
     }
 }
 
